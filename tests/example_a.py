7699fd5add07e32910134f85b059a77575345647
Chiedo <chiedo@chie.do>
2015-02-16 10:16:22 -0500

    Less redundant

--- tests/example_a.py
+++ tests/example_a.py
@@ -5,17 +5,6 @@ from tests.base_test import BaseTest
 
 class Test(BaseTest):
     def __init__(self, driver, base_url, module):
-        """Init
-
-        Parameters
-        ----------
-        driver : object
-            The selenium web driver
-        base_url : string
-            The base url of the web page we will be visiting.
-        module
-            The module currently being executed
-        """
         super(Test, self).__init__(driver, base_url, module)
 
     def run(self):
