979be143061eb2f33e4d3e52289b4fcf955119fe
Chiedo <chiedo@chie.do>
2015-02-17 07:46:28 -0500

    Browserstack no longer the default

--- README.md
+++ README.md
@@ -1,16 +1,42 @@
-Python-browserstack-selenium-starter
+Python-selenium-starter
 =========
 Starter set up for writing selenium tests with Python. The Goal is to make it easier to write and run tests
 using Selenium for multiple browsers and devices. With this repo, you should be able to just start writing your tests
-and not be concerned with the basic set up of your test framework. This is built for using browser stack but could
-easily be modified to not use browserstack with some basic coding.
+and not be concerned with the basic set up of your test framework. This has options to also run on browserstack but could easily be modified to use another service with some elementary coding.
 
 Installing Selenium
 ----------
 - Install pip first if needed obviously (google is your friend here)
 - pip install -U selenium
 
-Setting Up Your Environment Variables
+
+Getting Started
+-----------
+- You can add more tests by adding modules to the tests directory. Copy the structure in the examples and your tests
+should be executed automagically by the above.
+- You will likely want to update the data in the config.py file. But should not need to update anything in the
+run.py file unless you are applying a general fix to this repo that you plan to contribute..
+
+General Examples
+---------
+To run all tests
+```
+python run.py
+```
+To override the base url you set in run.py
+```
+python run.py --base_url "http://google.com"
+````
+To only run one test
+```
+python run.py --test "example_a.py"
+````
+To see more options
+```
+python run.py --help
+```
+
+Getting Started with Browserstack
 ----------
 - Before anything, you need to get your Selenium automate username and automate value from the following URL.https://www.browserstack.com/accounts/automate
 - Once you have those values modify your ~/.bash_profile to end with the following. (use ~/.bashrc instead if on linux)
@@ -23,8 +49,6 @@ export SELENIUM_AUTOMATE_VALUE="REPLACE_THIS"
 source ~/.bash_profile
 ```
 
-Getting Started
-----------
 Open a tab and run the following to start the local testing server. That will make sure you can still access the site via the correct IP address, etc.
 ```
 ./local_testing_binaries/osx/BrowserStackLocal $SELENIUM_AUTOMATE_VALUE localhost,3000,0
@@ -33,44 +57,28 @@ Then Run the following to execute the tests and hook into the browser stack loca
 ```
 python run.py --use_local
 ```
-If you want to skip browserstack altogether and just run tests on your personal machine, then run the following instead. This option is much faster. Currently, this defaults to Firefox on your personal machine.
-```
-python run.py --browserstack_off
-```
-- You can add more tests by adding modules to the tests directory. Copy the structure in the examples and your tests
-should be executed automagically by the above.
-- You will likely want to update the data in the config.py file. But should not need to update anything in the
-run.py file unless you are applying a general fix to this repo that you plan to contribute..
 
-Examples (Be sure to set the use_local flag if needed. And obviously, these arguments can be chained)
----------
+Browserstack Examples
+--------
 To run with only in the desktop browsers:
 ```
-python run.py --mobile
+python run.py --browserstack --mobile
 ```
 To run with only the in the desktop browsers:
 ```
-python run.py --desktop
+python run.py --browserstack --desktop
 ```
 To override the base url you set in run.py
 ```
 python run.py --base_url "http://google.com"
 ````
-To only run one test
-```
-python run.py --test "example_a.py"
-````
 To only run in one browser
 ```
-python run.py --capabilities "{'os_version': '7', 'browser_version': '8.0', 'os': 'Windows', 'resolution': '1024x768', 'browser': 'IE'}"
+python run.py --browserstack --capabilities "{'os_version': '7', 'browser_version': '8.0', 'os': 'Windows', 'resolution': '1024x768', 'browser': 'IE'}"
 ````
-To see more options
-```
-python run.py --help
-```
 
-Notes
----------
-- You can see more details here https://www.browserstack.com/automate/python.
+Browserstack Notes
+----------
+- For more browserstack options, you can see more details here https://www.browserstack.com/automate/python.
 - You will notice that your username and value are hard coded in those examples though. Just replace those areas with the environment variables we set up to make your code more shareable.
 - Local testing is VERY slow... Do as you wish
--- run.py
+++ run.py
@@ -6,19 +6,18 @@ import argparse
 import ast
 import time
 
-start_time = time.time()
-
 # Set up the command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--desktop", help="(boolean) Make the tests only run tests on desktop computers.",
+parser.add_argument("--desktop", help="(boolean) Make the tests only run tests on desktop computers. BROWSERSTACK.",
                     action="store_true")
-parser.add_argument("--mobile", help="(boolean) Make the tests only run tests on mobile devices.", action="store_true")
-parser.add_argument("--use_local", help="(boolean) Use the Local Browserstack selenium testing.",
+parser.add_argument("--mobile", help="(boolean) Make the tests only run tests on mobile devices. BROWSERSTACK",
+                    action="store_true")
+parser.add_argument("--use_local", help="(boolean) Use the Local Browserstack selenium testing. BROWSERSTACK",
                     action="store_true")
 parser.add_argument("--base_url", help="A way to override the DEFAULT_BASE_URL for your tests.",)
 parser.add_argument("--test", help="Only run one test as specified", default="all")
-desired_cap_config_notes = " The available values you can pass to this command will depend on the values you have\
-                           set in your config.py file"
+desired_cap_config_notes = "The available values you can pass to this command will depend on the values you have\
+                           set in your config.py file. BROWSERSTACK."
 parser.add_argument("--browser", help="Desktop: Only run tests on the given browser." +
                     desired_cap_config_notes, default=False)
 parser.add_argument("--browser_version", help="Desktop: Only run tests on the given browser version." +
@@ -38,7 +37,7 @@ parser.add_argument("--device", help="Mobile: Only run tests on the given the mo
 parser.add_argument("--capabilities", help="Example: \"{'browser': 'IE', 'browser_version': '8.0', 'os': 'Windows', " +
                     "'os_version': '7', 'resolution': '1024x768'}\" Can be used as an alternative to adding many of" +
                     "the arguments above such as mobile, desktop, browser, browser_version, etc.")
-parser.add_argument("--browserstack_off", help="This will instead run tests locally on firefox. Most of " +
+parser.add_argument("--browserstack", help="This will make tests execute on the browserstack platform instead" +
                     "the above flags will be ignored", action="store_true")
 args = parser.parse_args()
 
@@ -51,7 +50,7 @@ if(args.base_url is not None):
 else:
     BASE_URL = DEFAULT_BASE_URL
 
-if(args.browserstack_off is False):
+if(args.browserstack):
     # Grab the authentication variables from the environment
     try:
         selenium_username = os.environ['SELENIUM_AUTOMATE_USERNAME']
@@ -65,92 +64,99 @@ if(BASE_URL == ""):
     print "You need to set your DEFAULT_BASE_URL in config.py."
     exit()
 
-if(args.capabilities is not None):
-    # If the user passed --capabilities "{...}" then that will be the only platform tested on
+# If browserstack is set to be used then do set up the desired capabilities array, etc.
+if(args.browserstack):
+    if(args.capabilities is not None):
+        # If the user passed --capabilities "{...}" then that will be the only platform tested on
 
-    # convert the string into a python dictionary so it can be used in as a desired capability
-    desired_cap_list = [ast.literal_eval(args.capabilities)]
-else:
-    # If the user did not pass the --capabilities argumen then run all the tests listed below
+        # convert the string into a python dictionary so it can be used in as a desired capability
+        desired_cap_list = [ast.literal_eval(args.capabilities)]
+    else:
+        # If the user did not pass the --capabilities argumen then run all the tests listed below
 
-    # This has been set as an array so that we can set multiple environments and run the tests below in a for loop
-    # to repeat the tests for different operating systems. I have set a mobile capability list and
-    # desktop capability list to allow the use of either or with a flag. More tests can easily be added. See the
-    # browser stack website to get more options.
-    # The below tests IE8, IE9, IE10 and IE11 on windows and then chrome, safari and Firefox on a Mac. Pay attention to
-    # the version numbers.
-    desktop_cap_list = DESKTOP_CAP_LIST_CONFIGS
+        # This has been set as an array so that we can set multiple environments and run the tests below in a for loop
+        # to repeat the tests for different operating systems. I have set a mobile capability list and
+        # desktop capability list to allow the use of either or with a flag. More tests can easily be added. See the
+        # browser stack website to get more options.
+        # The below tests IE8, IE9, IE10 and IE11 on windows and then chrome, safari and Firefox on a Mac.
+        # Pay attention to the version numbers.
+        desktop_cap_list = DESKTOP_CAP_LIST_CONFIGS
 
-    # The below tests an iPhone 5 and Samsung Galaxy S5. If your Selenium Automate plan doesn't include Mobile, you will
-    # Want to change the following to mobile_cap_list = []
-    mobile_cap_list = MOBILE_CAP_LIST_CONFIGS
+        # The below tests an iPhone 5 and Samsung Galaxy S5. If your Selenium Automate plan doesn't include Mobile,
+        # you will Want to change the following to mobile_cap_list = []
+        mobile_cap_list = MOBILE_CAP_LIST_CONFIGS
 
-    # Conditionally create the desired_cap_list list. Didn't use elseif statements to allow for more user error
-    desired_cap_list = []
-    if(args.desktop):
-        # If the desktop argument has been passed, then only run the desktop tests
-        desired_cap_list = desired_cap_list + desktop_cap_list
-    if(args.mobile):
-        # If the mobile argument has been passed, then only run the mobile tests
-        desired_cap_list = desired_cap_list + mobile_cap_list
-    if(desired_cap_list == []):
-        # If no desktop or mobile argument has been passed, then run both the desktop and mobile tests
-        desired_cap_list = desktop_cap_list + mobile_cap_list
-
-# If a specific filter arg was set via the command line, remove anything from the desired_capabilites list that does not
-# meet the requirement
-desired_cap_list_filters = [
-    "os",
-    "browser",
-    "browser_version",
-    "resolution",
-    "os_version",
-    "browserName",
-    "platform",
-    "device",
-]
-# Loops through the abev list to do this to prevent duplicate code. You will notice that append was used below, instead
-# of remove. Remove was not acting as hoped so to cut corners, I just did this with addition instead of subtraction
-# to the same affect.
-for the_filter in desired_cap_list_filters:
-    if(getattr(args, the_filter) is not False):
-        temp_list = desired_cap_list
+        # Conditionally create the desired_cap_list list. Didn't use elseif statements to allow for more user error
         desired_cap_list = []
-        for desired_cap in temp_list:
-            if(the_filter in desired_cap and desired_cap[the_filter] == getattr(args, the_filter)):
-                desired_cap_list.append(desired_cap)
+        if(args.desktop):
+            # If the desktop argument has been passed, then only run the desktop tests
+            desired_cap_list = desired_cap_list + desktop_cap_list
+        if(args.mobile):
+            # If the mobile argument has been passed, then only run the mobile tests
+            desired_cap_list = desired_cap_list + mobile_cap_list
+        if(desired_cap_list == []):
+            # If no desktop or mobile argument has been passed, then run both the desktop and mobile tests
+            desired_cap_list = desktop_cap_list + mobile_cap_list
 
-# Completely override desired_cap list if the --browserstack_off argument was passed
-if(args.browserstack_off): desired_cap_list = [{"browser": "Firefox"}]
+    # If a specific filter arg was set via the command line, remove anything from the desired_capabilites list that
+    # does not meet the requirement
+    desired_cap_list_filters = [
+        "os",
+        "browser",
+        "browser_version",
+        "resolution",
+        "os_version",
+        "browserName",
+        "platform",
+        "device",
+    ]
+    # Loops through the abev list to do this to prevent duplicate code. You will notice that append was used below,
+    # instead of remove. Remove was not acting as hoped so to cut corners, I just did this with addition instead of
+    # subtraction
+    # to the same affect.
+    for the_filter in desired_cap_list_filters:
+        if(getattr(args, the_filter) is not False):
+            temp_list = desired_cap_list
+            desired_cap_list = []
+            for desired_cap in temp_list:
+                if(the_filter in desired_cap and desired_cap[the_filter] == getattr(args, the_filter)):
+                    desired_cap_list.append(desired_cap)
+# If the --browserstack argument was not passed, just set one element in the desired_cap_list for the sake
+# of using the same logic below. When looping through the elements of the desired_cap_list
+else:
+    desired_cap_list = [{"browser": "Firefox"}]
 
 # This will run the the same test code in multiple environments
 for desired_cap in desired_cap_list:
-    # Use browser stack local testing if told to do so
-    if(args.use_local):
-        desired_cap['browserstack.local'] = True
-    # Output a line to show what enivornment is now being tested
-    if("browser" in desired_cap and args.browserstack_off is True):
+
+    # If the browserstack argument was passed, then use the folowing to output the test headers
+    if(args.browserstack):
+        # Use browser stack local testing if the argument was passed
+        if(args.use_local): desired_cap['browserstack.local'] = True
+
+        # Output a line to show what enivornment is now being tested
+        if("browser" in desired_cap):
+            # For desktop on browserstack
+            print "\nStarting Tests on %s %s on %s %s with a screen resolution of %s " % (desired_cap["browser"],
+                desired_cap["browser_version"], desired_cap["os"], desired_cap["os_version"], desired_cap["resolution"])
+        else:
+            # For mobile on browserstack
+            print "\nStarting Tests on a %s" % (desired_cap["device"])
+    # Otherwise, just simply output this message
+    else:
         # For desktop on localmachine using firefox
         print "\nStarting Tests on %s on your local machine" % (desired_cap["browser"])
-    elif("browser" in desired_cap):
-        # For desktop on browserstack
-        print "\nStarting Tests on %s %s on %s %s with a screen resolution of %s " % (desired_cap["browser"],
-            desired_cap["browser_version"], desired_cap["os"], desired_cap["os_version"], desired_cap["resolution"])
-    else:
-        # For mobile on browserstack
-        print "\nStarting Tests on a %s" % (desired_cap["device"])
 
     print "--------------------------------------------------\n"
 
-    # YOUR TEST CODE SHOULD START HERE
-    # This should really just be calling test functions in test classes
-    # you set up though, for the sake of modularity
-    if(args.browserstack_off is True):
-        driver = webdriver.Firefox()
-    else:
+    # If the browserstack argument was passed, then dynamically set up the remote driver.
+    if(args.browserstack):
         driver = webdriver.Remote(
             command_executor="http://%s:%s@hub.browserstack.com:80/wd/hub" % (selenium_username, selenium_value),
             desired_capabilities=desired_cap)
+    # Otherwise, just run firefox locally.
+    else:
+        driver = webdriver.Firefox()
 
     tests_to_run = []
     if(args.test == "all"):
@@ -162,20 +168,36 @@ for desired_cap in desired_cap_list:
         # Otherwise, just add the one tests specified when passing the command
         tests_to_run = ["tests." + args.test.split(".")[0]]  # remove the .py from the test name
 
+    # The time when the test started
+    all_tests_start_time = time.time()
+
     # Loop through all the tests_to_run and runs them
     for test_to_run in tests_to_run:
+        # The time when the test started
+        this_test_start_time = time.time()
         # This dynamically imports all modules in the tests_to_run list. This allows me to import a module using
         # a variable. This is fairly advanced and hard to follow for the beginner.
         current_test = getattr(__import__(test_to_run, fromlist=["Test"]), "Test")
         test = current_test(driver, BASE_URL, test_to_run)
         test.run()
+
         # If it makes it this far, this means the test passed
         test.passed()
 
+        # Output the amount of time it took this test to run on the current platform
+        this_test_seconds_taken = time.time() - this_test_start_time
+        if(this_test_seconds_taken > 60):
+            print "Time taken: " + str(this_test_seconds_taken / 60) + " minutes"
+        else:
+            print "Time taken: " + str(this_test_seconds_taken) + " seconds"
+
+    # Output the amount of time it took all tests to run on the current platform
+    print "--------------------------------------------------\n"
+    all_tests_seconds_taken = (time.time() - all_tests_start_time)
+    if(all_tests_seconds_taken > 60):
+        print "Time taken for all tests: " + str(all_tests_seconds_taken / 60) + " minutes"
+    else:
+        print "Time taken for all tests: " + str(all_tests_seconds_taken) + " seconds"
+
     # Clean Up
     driver.quit()
-
-    # Output the number of minutes the tests took to run
-    print "Time taken: "
-    print (time.time() - start_time) / 60
-    print " minutes"
